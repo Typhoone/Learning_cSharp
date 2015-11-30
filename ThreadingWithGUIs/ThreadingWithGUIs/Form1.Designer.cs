52304dd3be8a4090b11ea3b5698533638e003743
Gordon Adams <gordonadamsnz@gmail.com>
2015-11-30 15:45:05 +1300

    Got threading working with the UI and diffrent classes

--- ThreadingWithGUIs/ThreadingWithGUIs/Form1.Designer.cs
+++ ThreadingWithGUIs/ThreadingWithGUIs/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.updateBox = new System.Windows.Forms.TextBox();
+            this.mousePos = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // updateBox
+            // 
+            this.updateBox.Location = new System.Drawing.Point(81, 31);
+            this.updateBox.Name = "updateBox";
+            this.updateBox.Size = new System.Drawing.Size(100, 20);
+            this.updateBox.TabIndex = 0;
+            this.updateBox.TextChanged += new System.EventHandler(this.updateBox_TextChanged);
+            // 
+            // mousePos
+            // 
+            this.mousePos.AutoSize = true;
+            this.mousePos.Location = new System.Drawing.Point(119, 99);
+            this.mousePos.Name = "mousePos";
+            this.mousePos.Size = new System.Drawing.Size(22, 13);
+            this.mousePos.TabIndex = 1;
+            this.mousePos.Text = "0,0";
+            this.mousePos.Click += new System.EventHandler(this.mousePos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.mousePos);
+            this.Controls.Add(this.updateBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox updateBox;
+        private System.Windows.Forms.Label mousePos;
     }
 }
 
--- ThreadingWithGUIs/ThreadingWithGUIs/Form1.cs
+++ ThreadingWithGUIs/ThreadingWithGUIs/Form1.cs
@@ -5,16 +5,101 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ThreadingWithGUIs
 {
+   
+
     public partial class Form1 : Form
     {
+
+        //Delegate sig
+        public delegate void delTextBoxUpdate(string text);
+
+        ThreadStart threadStart;
+        Thread updateThread;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void updateBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            threadStart = new ThreadStart(startThread);
+            updateThread = new Thread(threadStart);
+            updateThread.Name = "secondThread";
+
+            updateThread.Start();
+
+            new mouseControl(this.mousePos);
+
+        }
+
+
+        private void startThread()
+        {
+            delTextBoxUpdate updator = new delTextBoxUpdate(updateTextBox);
+            for(int i = 0; i < 1000; i++)
+            {
+                this.updateBox.BeginInvoke(updator, i + "");
+                Thread.Sleep(100);
+            }
+        }
+
+        private void updateTextBox(string String)
+        {
+            this.updateBox.Text = String;
+        }
+
+        private void mousePos_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+    public class mouseControl
+    {
+        public delegate void delLableUpdate(string text);
+
+        int ID;
+        Thread t;
+        Label label;
+
+        public mouseControl(Label l)
+        {
+            ID = new Random().Next(1, 100000);
+            t = new Thread(new ThreadStart(watchMouse));
+            this.label = l;
+
+            t.Start();
+        }
+
+        public void watchMouse()
+        {
+            delLableUpdate updator = new delLableUpdate(updateLabel);
+
+            while (true)
+            {
+                this.label.BeginInvoke(updator, Cursor.Position.X + "," + Cursor.Position.Y);
+                Thread.Sleep(10);
+            }
+        }
+
+        public void updateLabel(string text)
+        {
+            this.label.Text = text;
         }
     }
 }
